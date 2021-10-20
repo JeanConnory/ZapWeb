08ad6dcdfda8711d54c011db26396c84a675b4a5
Jean Connory <michaelrhcp@gmail.com>
2021-10-19 22:40:27 -0300

    com o projeto xamarin forms

---
+++ ZapApp/ZapApp.Android/SplashActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZapApp.Droid
+{
+    [Activity(Label = "ZapApp", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
